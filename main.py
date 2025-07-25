784d6c62354d4779c97647d25a80c39b563bc7c8
Alexandre Guenot <realnameherej@gmail.com>
2025-07-25 14:50:06 +0200

    add data

---
+++ main.py
@@ -0,0 +1,2 @@
+from my_data import vegetables, fruits
+import random
---
+++ my_data.py
@@ -0,0 +1,24 @@
+vegetables = [
+    "Spinach",
+    "Carrot",
+    "Broccoli",
+    "Bell Pepper",
+    "Zucchini",
+    "Cucumber",
+    "Tomato",
+    "Onion",
+    "Garlic",
+    "Kale"
+],
+fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Strawberry",
+    "Grapes",
+    "Blueberry",
+    "Mango",
+    "Pineapple",
+    "Watermelon",
+    "Kiwi"
+]
